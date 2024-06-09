90140f902172e56f2ccbec3ba988bc074a7ce664
Richard Souwiko <richard.s050804@gmail.com>
2024-06-09 21:08:32 +0700

    setup category page

---
+++ Zest/app/Http/Controllers/CategoryController.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Models\Category;
+
+class CategoryController extends Controller
+{
+    //
+    public function index() {
+        $category = Category::all();
+
+        return view('categories.index', compact('category'));
+    }
+}
---
+++ Zest/database/seeders/CategorySeeder.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class CategorySeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public function run(): void
+    {
+        //
+        DB::table('category')->insert([
+            'kategori' => 'Kategori 1',
+            'jumlah' => 20,
+        ]);
+    }
+}
