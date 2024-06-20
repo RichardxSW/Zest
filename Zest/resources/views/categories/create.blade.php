fbc2c2f5dfb53053763eab93492a735e059781d6
Richard Souwiko <richard.s050804@gmail.com>
2024-06-20 14:01:35 +0700

    ubah page create & edit jd modal

--- Zest/resources/views/categories/create.blade.php
+++ Zest/resources/views/categories/create.blade.php
@@ -1,35 +1,42 @@
-@extends('layouts.template')
+<style>
+    .mb-3 {
+        margin-bottom: 30px !important;
+    }
+</style>
 
-@push('styles')
-<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-@endpush
-
-@section('content')
-<section class="page-section bg-light" id="portfolio">
-            <div class="container">
-            <h2 class="mt-5">ADD NEW CATEGORY</h2>
+<div class="modal fade" id="addCategoryModal" tabindex="-1" role="dialog" aria-labelledby="addCategoryModalLabel" aria-hidden="true">
+    <div class="modal-dialog custom-modal-size modal-dialog-centered" role="document">
+        <div class="modal-content custom-modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="addCategoryModalLabel">Add New Category</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
                 <form action="{{ route('categories.store') }}" method="POST">
                     @csrf
-                <div class="mb-3">
-                    <label class="form-label">Category</label>
-                    <input type="text" class="form-control" name="kategori">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Quantity</label>
-                    <input type="text" class="form-control" name="jumlah">
-                </div>
-               <!--  <div class="mb-3">
-                    <label class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Jurusan</label>
-                    <input type="text" class="form-control" name="jurusan">
-                </div> -->
+                    <div class="mb-3">
+                        <label class="form-label">Category</label>
+                        <input type="text" class="form-control" name="kategori">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Quantity</label>
+                        <input type="text" class="form-control" name="jumlah">
+                    </div>
+                    <!-- <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="text" class="form-control" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Phone</label>
+                        <input type="text" class="form-control" name="phone">
+                    </div> -->
 
-                <button type="submit" class="btn btn-primary">Submit</button>
-                <a href="{{ route('categories.index') }}" class="btn btn-danger">Cancel</a>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success">Add</button>
                 </form>
             </div>
-        </section>
-@endsection
+        </div>
+    </div>
+</div>
--- Zest/resources/views/categories/edit.blade.php
+++ Zest/resources/views/categories/edit.blade.php
@@ -1,36 +1,45 @@
-@extends('layouts.template')
+<style>
+    .mb-3 {
+        margin-bottom: 30px !important;
+    }
+</style>
 
-@push('styles')
-<link href="{{ asset('/css/style.css') }}" rel="stylesheet">
-@endpush
-
-@section('content')
-<section class="page-section bg-light" id="portfolio">
-            <div class="container">
-            <h2 class="mt-5">EDIT CATEGORY</h2>
+@foreach ($category as $cat)
+<div class="modal fade" id="editCategoryModal{{ $cat->id }}" tabindex="-1" role="dialog" aria-labelledby="editCategoryModalLabel{{ $cat->id }}" aria-hidden="true">
+    <div class="modal-dialog custom-modal-size modal-dialog-centered" role="document">
+        <div class="modal-content custom-modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="editCategoryModalLabel{{ $cat->id }}">Edit Category </h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                    <span aria-hidden="true">&times;</span>
+                </button>
+            </div>
+            <div class="modal-body">
                 <form action="{{ route('categories.update', $cat->id) }}" method="POST">
-                    @method('PUT')
                     @csrf
-                <div class="mb-3">
-                    <label class="form-label">Category</label>
-                    <input type="text" class="form-control" name="kategori" value="{{ $cat->kategori }}">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Quantity</label>
-                    <input type="text" class="form-control" name="jumlah" value="{{ $cat->jumlah }}">
-                </div>
-               <!--  <div class="mb-3">
-                    <label class="form-label">Email</label>
-                    <input type="email" class="form-control" name="email">
-                </div>
-                <div class="mb-3">
-                    <label class="form-label">Jurusan</label>
-                    <input type="text" class="form-control" name="jurusan">
-                </div> -->
+                    @method('PUT')
+                    <div class="mb-3">
+                        <label class="form-label">Category</label>
+                        <input type="text" class="form-control" name="kategori" value="{{ $cat->kategori }}">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Quantity</label>
+                        <input type="text" class="form-control" name="jumlah" value="{{ $cat->jumlah }}">
+                    </div>
+                    <!-- <div class="mb-3">
+                        <label class="form-label">Email</label>
+                        <input type="text" class="form-control" name="email">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Phone</label>
+                        <input type="text" class="form-control" name="phone">
+                    </div> -->
 
-                <button type="submit" class="btn btn-primary">Submit</button>
-                <a href="{{ route('categories.index') }}" class="btn btn-danger">Cancel</a>
+                    <button type="button" class="btn btn-danger" data-dismiss="modal">Cancel</button>
+                    <button type="submit" class="btn btn-success">Update</button>
                 </form>
             </div>
-        </section>
-@endsection
+        </div>
+    </div>
+</div>
+@endforeach
