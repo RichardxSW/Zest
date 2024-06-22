8e423bfaed41c46b494e2104f94e7644a2e416c7
TanjayaJason <tanjaya.jason01@gmail.com>
2024-06-22 13:54:14 +0700

    update alert

--- Zest/resources/views/supplier/index.blade.php
+++ Zest/resources/views/supplier/index.blade.php
@@ -21,6 +21,13 @@
             </form>
         </div>
     </div>
+
+    @if (session('success'))
+        <div class="alert alert-success alert-dismissible fade show" role="alert">
+            <strong>{{ session('success') }}</strong>
+            <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+    @endif
     
     <table class="table table-bordered table-striped">
         <thead>
