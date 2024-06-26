6bc8afd7df514e6ca40cc12776b7cd35299a6649
Richard Souwiko <richard.s050804@gmail.com>
2024-06-26 23:54:53 +0700

    add total sales at product table

---
+++ Zest/database/migrations/2024_06_26_104513_add_total_sales_to_product_table.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+            $table->integer('total_sales')->default(0);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('product', function (Blueprint $table) {
+            //
+            $table->dropColumn('total_sales');
+        });
+    }
+};
